7aca0abe68a5caf44e6e1488128fe1ded924302c
I_AM_TINE <sutichok46@gmail.com>
2022-06-27 16:19:25 +0700

    4-template-for-workshop

--- deploy/compiler.js
+++ deploy/compiler.js
@@ -2,9 +2,6 @@ const path = require('path')
 const fs = require('fs')
 const solc = require('solc')
 
-// get inbox.sol contract from folder contracts
-const contractPath = path.resolve(__dirname,"contracts","inbox.sol")
-const inboxSource = fs.readFileSync(contractPath,"utf-8")
 
 // this is compiler input
 const input = {
@@ -23,12 +20,8 @@ const input = {
     }
 }; 
 
-// compile Solidity into bytecode
-const compiled = JSON.parse(solc.compile(JSON.stringify(input))).contracts["Inbox.sol"]["Inbox"]
 
-// abi is like a meta data of our contract
-const abi = compiled.abi
-// bytecode of this contract
-const bytecode = compiled.evm.bytecode.object
+
+
 
 module.exports = {abi,bytecode}
